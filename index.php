204c4118bbd5bcbfacd977b21b027d5929b00e4f
ssor <rewallings@gmail.com>
2024-09-10 20:00:07 +0100

    added svgs, converted to PHP and started building header nav bar

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title><?= 'Ross Wallings - Software Developer' ?></title>
+        <link href="default.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+    <?php include(__DIR__."/includes/header.php"); ?>
+
+    <div class="info">
+    
+    </div>
+
+    </body>
+
+    </html>
